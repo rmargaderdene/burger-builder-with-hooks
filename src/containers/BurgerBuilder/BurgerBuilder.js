6441efac3ebab235fb1ed83f7506879cbf6286cf
margad <rmargaderdene@gmail.com>
2018-03-12 07:17:55 +0800

    after adding burgerIngredient module

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,24 @@ import React, { Fragment, Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...};
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
   render () {
       return (
         <Fragment>
-          <Burger/>
+          <Burger ingredients={this.state.ingredients}/>
           <div>Build Controls</div>
         </Fragment>
       );
